78d3316ea5ddf070b4fdee83c5f0f28b5e984b3d
Zhannur <jannuraidynuly@gmail.com>
2021-12-17 09:40:42 +0500

    Updates

---
+++ precache-manifest.ef069539e99feaa02b78763ea7a3a2e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee84a2faf57bf0fe9c18",
+    "url": "/first_react/static/css/main.8b3c4fc0.chunk.css"
+  },
+  {
+    "revision": "ee84a2faf57bf0fe9c18",
+    "url": "/first_react/static/js/main.ee84a2fa.chunk.js"
+  },
+  {
+    "revision": "08afdcdac48f9df8e978",
+    "url": "/first_react/static/js/1.08afdcda.chunk.js"
+  },
+  {
+    "revision": "08bd1f23d226d236f69f",
+    "url": "/first_react/static/js/runtime~main.08bd1f23.js"
+  },
+  {
+    "revision": "6c696d7e0ffa3e86070772d4f1b6d7db",
+    "url": "/first_react/index.html"
+  }
+];
